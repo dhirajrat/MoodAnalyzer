9043ab7db68431aeea6b656c47d143e599c94054
dhirajrathod <dhirajrathod3000@gmail.com>
2021-08-06 17:48:21 +0530

    ADDED: Welcome to Mood Analyzer

---
+++ src/main/java/MoodAnalyzer.java
@@ -0,0 +1,13 @@
+public class MoodAnalyzer {
+
+    public String analyzeMood(String message){
+
+
+        if(message.contains("sad")){
+            return "SAD";
+        }
+        else {
+            return "HAPPY";
+        }
+    }
+}
